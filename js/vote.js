b7e479b7a484dfd99dc1d37bd920ef360b67a0cc
Luke Hardiman <luke@hardiman.co.nz>
2015-07-05 16:18:49 +1200

    Push tested final
    This is a working testing version off myvote

---
+++ js/vote.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+    var vote_details = {
+        party : null,
+        mp : null
+    }
+
+    $(".portfolio-box").click(function(){
+        var party_name = $(this).attr('id');
+        vote_details.party = party_name;
+
+        console.log("party_name",party_name)
+        $(this).css('opacity','0.2');
+        $(this).parent().addClass("fa fa-4x fa-check");
+        $(".portfolio-box" ).unbind("click");
+    })
+
+    $(".mp-vote").click(function(){
+        var mp_vote = $(this).attr('mp_name');
+        console.log("mp_vote",mp_vote);
+        vote_details.mp = mp_vote;
+        $(".mp-vote").unbind("click");
+    })
+
+    $("#submitVote").click(function(){
+        console.log("vote_details",vote_details);
+        $.ajax({
+            url : "vote_api.php?recordVote",
+            data : vote_details,
+            dataType : 'jsonp',
+            method : 'GET',
+            success : function(response) {
+                console.log("response",response);
+                if (response.success == true){
+                    alert("Voted, thank you");
+                    location.href = "index.html"
+                }else{
+
+                    alert("sorry you have already voted");
+                    location.href = "index.html"
+                }
+                console.log("response", response);
+            }
+
+        });
+    })
+})
+
+
+
+
+//vote_details
