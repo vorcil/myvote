80a84c428f440a7bcb3a7eaf428721c83bbf59d4
vorcil <rajol.kochlashvili@mail.ru>
2015-07-14 20:06:07 +1200

    update validate

--- realme/validateLogon.js
+++ realme/validateLogon.js
@@ -1,1 +1,11 @@
-$(document).ready(function(){$("#logon").validate({submitHandler:handleSubmit,rules:{"logon:username:input":usernameValidation.rules.existingUsername,"logon:userPassword:input":passwordValidation.rules.existingPassword,"logon:igovtcode":sixDigitCodeValidator.rules.code},messages:{"logon:username:input":usernameValidation.messages.existingUsername,"logon:userPassword:input":passwordValidation.messages.existingPassword}})});
+$(document).ready(function(){
+
+$("#logon").validate({
+submitHandler:handleSubmit,rules:{
+
+"logon:username:input":usernameValidation.rules.existingUsername,"logon:userPassword:input":passwordValidation.rules.existingPassword,"logon:igovtcode":sixDigitCodeValidator.rules.code
+},messages:{
+
+"logon:username:input":usernameValidation.messages.existingUsername,"logon:userPassword:input":passwordValidation.messages.existingPassword
+}})
+});
